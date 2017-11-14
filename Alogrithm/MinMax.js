b842c8024a47cbfbfd9aa3d56fec067d73fe0d52
Rahul Mishra <rahulmishra240291@gmail.com>
2017-11-14 17:29:49 +0530

    finalAnswer

--- Alogrithm/MinMax.js
+++ Alogrithm/MinMax.js
@@ -23,5 +23,16 @@ function readLine() {
 function main() {
     arr = readLine().split(' ');
     arr = arr.map(Number);
+    var total=arr[0], min=arr[0], max=arr[0];
+    for (i=1; i<arr.length; i++) {
+        total = total+arr[i];
+        if (min>arr[i]) {
+            min=arr[i];
+        }
+        if (max<arr[i]) {
+            max=arr[i];
+        }
+    }
+    console.log(total-max, total-min)
 
 }
