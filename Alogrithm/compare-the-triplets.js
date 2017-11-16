5dbd72781835eba0cd33b310595a0296a6b1ce86
Rahul Mishra <rahulmishra240291@gmail.com>
2017-11-16 15:01:16 +0530

    WarmUp

---
+++ Alogrithm/compare-the-triplets.js
@@ -0,0 +1,52 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function solve(a0, a1, a2, b0, b1, b2){
+    // Complete this function
+  
+ var aliceScore = 0;
+    var bobScore = 0;
+    if(a0 > b0){aliceScore++;}
+    if(a1 > b1){aliceScore++;}
+    if(a2 > b2){aliceScore++;}    
+    if(a0 < b0){ bobScore++;}
+    if(a1 < b1){bobScore++;}
+    if(a2 < b2){bobScore++;}    
+    
+    return console.log(aliceScore,bobScore);
+}
+
+function main() {
+    var a0_temp = readLine().split(' ');
+    var a0 = parseInt(a0_temp[0]);
+    var a1 = parseInt(a0_temp[1]);
+    var a2 = parseInt(a0_temp[2]);
+    var b0_temp = readLine().split(' ');
+    var b0 = parseInt(b0_temp[0]);
+    var b1 = parseInt(b0_temp[1]);
+    var b2 = parseInt(b0_temp[2]);
+    var result = solve(a0, a1, a2, b0, b1, b2);
+    console.log(result.join(" "));
+    
+
+
+}
